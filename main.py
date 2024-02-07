b75c6f747c6ff61d1c10ded2b25f0ada392754ab
matthew4301 <matthewstothard43@gmail.com>
2024-02-07 09:28:48 +0000

    implemented displaying question into the gui

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 import pygame_gui
 import projectile_simulator
-import teach
 import settings
 import questions
 import load
--- questions.py
+++ questions.py
@@ -12,12 +12,11 @@ background = pygame.Surface((800, 600))
 background.fill(pygame.Color('#787878'))
 black = (0, 0, 0)
 font = pygame.font.SysFont("Comic Sans MS", 40)
-font2 = pygame.font.SysFont("Comic Sans MS", 20)
+font2 = pygame.font.SysFont("Comic Sans MS", 15)
 manager = pygame_gui.UIManager((800, 600))
 
 questions = []
 answers = []
-question = "------------"
 
 class Buttons():
     def __init__(self):
@@ -26,9 +25,9 @@ class Buttons():
 
     def checkpressed(self,button):
         if button == self.quit:
-            is_running = False
+            return False
         if button == self.start:
-            question = find_question()
+            return True, find_question()
 
 def load_db():
     with sqlite3.connect("saves/database.db") as db:
@@ -36,8 +35,8 @@ def load_db():
     cursor.execute('''
 CREATE TABLE IF NOT EXISTS Questions(
 QuestionID INTEGER PRIMARY KEY,
-Question TEXT NOT NULL,
-Answer FLOAT NOT NULL);''')
+Answer TEXT NOT NULL,
+Question FLOAT NOT NULL);''')
     db.commit()
     cursor.execute('''
 CREATE TABLE IF NOT EXISTS Progress(
@@ -57,25 +56,25 @@ def load_questiontxt():
         list = [line.strip() for line in file.readlines()]
     for i in range(len(list)):
         if i % 2 == 0:
-            answers.append(list[i])
-        else:
             questions.append(list[i])
+        else:
+            answers.append(list[i])
 
 def select_randomquestion():
     range = len(questions)-1
-    question = random.randint(0,range)
-    return question
+    return random.randint(0,range)
 
 def find_question():
     with sqlite3.connect("saves/database.db") as db:
         cursor = db.cursor()
-    query = ("""
+    cursor.execute("""
 SELECT Question
 FROM Questions
-WHERE QuestionID = ?
-""")
-    cursor.execute(query,[(select_randomquestion())])
-    question = cursor.fetchall()  
+WHERE QuestionID = ?;
+""",[(select_randomquestion())])
+    question = str(cursor.fetchall())
+    question.replace('[','')
+    question.replace(']','')
     return question
 
 def start():
@@ -83,6 +82,7 @@ def start():
     load_db()
     clock = pygame.time.Clock()
     is_running = True
+    question = ""
     while is_running:
         time_delta = clock.tick(60)/1000.0
         for event in pygame.event.get():
@@ -90,12 +90,12 @@ def start():
                 is_running = False
             manager.process_events(event)
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                is_running = b.checkpressed(event.ui_element)
+                is_running,question = b.checkpressed(event.ui_element)
         manager.update(time_delta)
         window_surface.blit(background, (0, 0))
         manager.draw_ui(window_surface)
         window_surface.blit(font.render("Questions", True, black, None),(300,75))
-        window_surface.blit(font2.render(question, True, black, None),(150,75))
+        window_surface.blit(font2.render(str(question), True, black, None),(10,150))
         pygame.display.update()
     return True
 
